ab4c3d60e2fa745706c43de5661a02ac0558ea81
Hryushek <ankolts@yandex.ru>
2022-06-06 18:57:29 +0300

    correct structure

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+const editButton = document.querySelector('.profile__edit-button');
+const popupForm = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__close-button');
+const likeButton = document.querySelector('.card__like-button');
+const nameInput = document.querySelector('#name');
+const jobInput = document.querySelector('#job');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__description');
+const saveButton = document.querySelector('.popup__save-button');
+
+function popupOpen() {
+    popupForm.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+}
+
+function popupClose() {
+    popupForm.classList.remove('popup_opened');
+}
+
+// function like() {
+//     likeButton.style.backgroundImage = "url(images/black_like.svg)";
+// }
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    popupClose();
+}
+
+editButton.addEventListener('click', popupOpen);
+
+closeButton.addEventListener('click', popupClose);
+
+// likeButton.addEventListener('click', like);
+
+saveButton.addEventListener('click', formSubmitHandler); 
