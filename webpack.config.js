5b1b69e79efefc57057a2fb17c9b1f5114c7ed94
Hryushek <ankolts@yandex.ru>
2022-09-26 17:43:51 +0300

    webpack init

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
-    entry: { main: './src/inndex.js' },
+    entry: { main: './src/pages/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
@@ -23,13 +24,26 @@ module.exports = {
             test: /\.js$/,
             use: 'babel-loader',
             exclude: '/node_modules/'
+          },
+          {
+            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+            type: 'asset/resource'
+          },
+          {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, {
+              loader: 'css-loader',
+              options: { importLoaders: 1 }
+            },
+            'postcss-loader']
           }
-          ]
+        ]
       },
       plugins: [
           new HtmlWebpackPlugin({
         template: './src/index.html'
       }),
       new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin()
     ]
 }
